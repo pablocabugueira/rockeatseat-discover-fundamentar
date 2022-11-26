c3a8e0ae7ee1467225509db2119dd27d99d2f629
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-11-25 22:14:56 -0300

    Create objects.js

---
+++ JS/tipos de dados/objects.js
@@ -0,0 +1,17 @@
+/* Object
+
+> Objeto
+> Propriedades / Atributos
+> Funcionalidades / Métodos
+
+{ Propriedade: "valor" }
+
+*/
+
+console.log({
+    name: "Pablo",
+    idade: 18,
+    andar: function() {
+        console.log("andar")
+    }
+})
