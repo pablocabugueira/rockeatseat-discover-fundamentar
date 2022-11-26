321c168154596684bb8040725d508def820bd71b
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-11-25 21:33:12 -0300

    Create numbers.js

---
+++ JS/numbers.js
@@ -0,0 +1,11 @@
+// Numbers
+
+// 33 - Número inteiro
+// 12.5 - Número real ou float
+// NaN - Não é um número
+// Infinity - Infinito
+
+console.log(`Aqui tem um número inteiro ${3} ou ${33}`)
+console.log(`Aqui tem um número real ou float ${3.2} ou ${33.5}`)
+console.log(`Aqui tem um NaN(Não é um número) ${15 / "advda"}`)
+console.log(`Aqui tem um Infinito ${15 === Infinity} ou ${Infinity}`)
