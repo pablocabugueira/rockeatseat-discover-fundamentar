15586c3cc23301f2aed1a168feb74ea54f1fdf3e
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-11-27 20:43:18 -0300

    Update scripts.js

---
+++ JS/scripts.js
@@ -0,0 +1,19 @@
+/* Variáveis
+
+var
+let
+const
+
+*/
+
+var clima = "quente"
+clima = "frio"
+console.log(clima)
+
+let idade = 13
+idade = 15
+console.log(idade)
+
+const nome = "Gabriel"
+// const não pode mudar.
+console.log(nome)
