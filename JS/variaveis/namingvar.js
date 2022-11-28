5f1fc43cdc5108a84ac0f3bf2f2b3fbe3bf0f76d
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-11-27 21:25:31 -0300

    Create namingvar.js

---
+++ JS/variaveis/namingvar.js
@@ -0,0 +1,43 @@
+/* Nomeando variáveis
+
+> JS é case-sensitive (identifica e diferencia se é maiúsculo ou minúsculo)
+> JS aceita Unicode (quase todas as caracteres)
+
+- Posso:
+    > Usar esses caracteres especiais: '& e _' */
+    let $$na$me_ = "pedrin"
+    console.log($$na$me_)
+
+//  > Usar acentos
+    let também = 15
+    console.log(também)
+
+/*
+- Não posso:
+    > Iniciar com números
+    let 1número = 2 > isso é errado
+
+    > Colocar espaços
+    let eu gosto de pipa = true > isso é errado
+
+- Ideal:
+    > Nomes que fazem sentido */
+    let idadeDoJoão = 18
+    console.log(idadeDoJoão)
+
+//  > Explicar o que é e o que a variável faz
+    let nome = "Kayo"
+    let checkIfNameIsKayo = nome === "Kayo"
+    console.log(checkIfNameIsKayo)
+
+// > camelCase - Quando fosse iniciar outra palavra colocar letra maiúscula
+    let euGostoDeVocê = true
+    console.log(euGostoDeVocê)
+
+// > snakCase - Quando fosse iniciar outra palavra colocar underline
+    let eu_gosto_de_você = false
+    console.log(eu_gosto_de_você)
+
+// Escrever em inglês
+
+// Isso facilita o aprendizado e treina o inglês
