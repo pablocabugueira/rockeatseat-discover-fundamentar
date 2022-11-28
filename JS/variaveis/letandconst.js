8a0b46b4b1f2dfb30546c1cca8e312e05a0b3bf9
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-11-27 21:05:30 -0300

    Create letandconst.js

---
+++ JS/variaveis/letandconst.js
@@ -0,0 +1,22 @@
+/* Let e Const
+
+> Só funcionam localmente.
+*/
+
+let nome = "pedro"
+
+{
+    let nome = "joao"
+    console.log(nome)
+}
+
+console.log(nome)
+
+const idade = 14
+
+{
+    const idade = 16
+    console.log(idade)
+}
+
+console;log(idade)
