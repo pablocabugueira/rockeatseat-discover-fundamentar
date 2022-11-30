fc32e6bb5658a553503bc6536f9b1842ce58f418
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-11-30 14:02:03 -0300

    Create concatandinterpolvar.js

---
+++ JS/pratica/concatandinterpolvar.js
@@ -0,0 +1,12 @@
+// Concatenando variáveis
+
+let name, age
+
+name = "Pablo"
+age = 18
+
+console.log("O " + name + " tem " + age + " anos.")
+
+//Interpolando variáveis
+
+console.log(`Após ${name} fazer ${age} anos, ele ganhou um presente.`)
