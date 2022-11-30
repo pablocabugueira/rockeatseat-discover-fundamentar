9458bfe0dd7846cb3ca71dcc0cede93cf9a33274
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-11-30 20:20:00 -0300

    Update functions.js

---
+++ JS/functions/functions.js
@@ -0,0 +1,17 @@
+// Declaration - Declaração
+
+function createPhrases() {
+    console.log("Você é um máximo!")
+    console.log("Você consegue!")
+    console.log("Acredita!")
+}
+
+// Executar, rodar, chamar, invocar
+// Execute, run, call, invoke
+
+createPhrases()
+createPhrases()
+createPhrases()
+createPhrases()
+
+console.log("Fim do programa!")
