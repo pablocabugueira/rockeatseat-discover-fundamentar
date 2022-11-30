c2dabf7b121e192c5541e777ae9ae343342b926c
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-11-30 14:36:38 -0300

    Create exercises.js

---
+++ JS/pratica/exercises.js
@@ -0,0 +1,62 @@
+// Desafios
+
+// 1
+let weight
+
+// 2
+console.log(typeof weight)
+
+// 3
+let name, age, stars, isSubscribed
+
+name = "Pablo"
+age = 18
+stars = 96.4
+isSubscribed = true
+
+//4
+//Object
+
+const student = {
+    name: "Pablo",
+    age: 18,
+    weight: 64.3,
+    isSubscribed: true
+}
+
+console.log(`${student.name} de idade ${student.age} pesa ${student.weight}Kg.`)
+
+// 5
+let students = []
+
+//6
+students = [
+    student
+]
+
+console.log(students)
+
+//7
+console.log(students[0])
+
+//8
+
+const student2 = {
+    name: "Fernando",
+    age: 19,
+    weight: 71.3,
+    isSubscribed: false
+}
+
+students = [
+    student,
+    student2
+]
+
+console.log(students[1])
+
+//9
+//Undefined
+
+//10
+//Error
