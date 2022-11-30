fc08156d2285e949f27c7908b58d7dd9e6ab3cb0
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-11-30 14:19:29 -0300

    Create array.js

---
+++ JS/pratica/array.js
@@ -0,0 +1,23 @@
+// Array
+
+const animals = [
+    "cow",
+    "cat",
+    "lion"
+]
+
+console.log(animals[0])
+
+// Pode ter objeto dentro de um array
+
+const animals2 = [
+    "cow",
+    "snake",
+    {
+        name: "elephant",
+        weight: 900
+    },
+    "dog"
+]
+
+console.log(animals2[2].name)
