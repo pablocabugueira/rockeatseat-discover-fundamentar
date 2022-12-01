d8eff184fecf5d0b0f941ee35789c4c7a33ad964
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-11-30 21:30:18 -0300

    Create functions_2.js

---
+++ JS/functions/functions_2.js
@@ -0,0 +1,10 @@
+// Function Expression
+// Function Anonymous
+
+// Parâmetros (parameters)
+
+const sum = function(number1, number2) {
+    console.log(number1 + number2)
+}
+
+sum(2, 3) // Argumentos - Arguments
