9830a4e79251bb6597905888b0d1c9529b79c92f
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-01 22:38:42 -0300

    Create functions_3.js

---
+++ JS/functions/functions_3.js
@@ -0,0 +1,15 @@
+let total = 0
+
+const sum = function(number1, number2) {
+    let total = number1 + number2
+    return total
+}
+
+let number1 = 5
+let number2 = 10
+
+console.log(`O primeiro número é ${number1}`)
+console.log(`O segundo número é ${number2}`)
+console.log(`A soma deles é ${sum(number1, number2)}`)
+
+console.log(total)
