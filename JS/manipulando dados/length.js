b27cbbbd1aef8237181363dbf025597978f98781
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-06 22:17:52 -0300

    Create length.js

---
+++ JS/manipulando dados/length.js
@@ -0,0 +1,9 @@
+// Contar quantas caracteres tem em uma palavra e em números
+
+let word = "paralelepipedo"
+
+console.log(word.length)
+
+let number = 1234
+
+console.log(String(number).length) // .length não funciona em number
