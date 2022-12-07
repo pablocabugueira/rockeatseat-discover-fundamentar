ac5e2c43367ad933c85a99523958a0039cb95d19
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-06 22:09:40 -0300

    Create typeConversionAndCoersion.js

---
+++ JS/manipulando dados/typeConversionAndCoersion.js
@@ -0,0 +1,7 @@
+// Type Conversion (typecasting) ou Type Coersion
+
+// Alteração de um tipo de dado para outro, exemplos:
+
+console.log("9" + 8) // Type Coersion, o 8 vira uma string
+
+console.log(Number("9") + 8) // Type Conversion, alteração manual de string para number
