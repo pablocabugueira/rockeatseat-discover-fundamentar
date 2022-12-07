e31f782ff2310eebbf147a259dc541ad2b4f1070
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-06 22:25:52 -0300

    Create lowerCaseAndUpperCase.js

---
+++ JS/manipulando dados/lowerCaseAndUpperCase.js
@@ -0,0 +1,7 @@
+// Tranformar a palavra para ter todas as letras maiúsculas ou minúsculas
+
+let word = "Eu Gosto de Futebol"
+
+console.log(word.toLowerCase()) // LowerCase
+
+console.log(word.toUpperCase()) // UpperCase
