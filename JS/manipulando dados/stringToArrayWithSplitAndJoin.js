e6d1151b59896f77cd664fe2a5c5cfb26a15991b
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-06 22:41:41 -0300

    Create stringToArrayWithSplitAndJoin.js

---
+++ JS/manipulando dados/stringToArrayWithSplitAndJoin.js
@@ -0,0 +1,15 @@
+// Manipulando string e array
+
+let phrase = "Eu Gosto de Futebol"
+
+let myArray = phrase.split(" ")
+
+console.log(myArray)
+
+let phraseWithUnderscore = myArray.join("_")
+
+console.log(phraseWithUnderscore)
+
+let phraseWithUnderscoreAndLowerCase = phraseWithUnderscore.toLowerCase()
+
+console.log(phraseWithUnderscoreAndLowerCase)
