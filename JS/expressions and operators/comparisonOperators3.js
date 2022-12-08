d59037e88462f79f38c9f85ae62b89ab100fcf79
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-07 22:46:43 -0300

    Create comparisonOperators3.js

---
+++ JS/expressions and operators/comparisonOperators3.js
@@ -0,0 +1,15 @@
+// Operadores de comparação
+// Compara valores e retorna um boolean como resultado
+// > Maior que
+// >= Maior ou igual
+// < Menor que
+// <= Menor ou igual
+
+let one = 1
+let two = 2
+
+console.log(one > 0) // False
+console.log(one >= 1) // True
+
+console.log(two < 3) // True
+console.log(two <= 1) // False
