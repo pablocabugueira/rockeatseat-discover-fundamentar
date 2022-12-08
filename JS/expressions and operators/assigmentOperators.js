b11ad48c1e5806d2cbdba990961ef583557a83fa
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-07 23:00:16 -0300

    Create assigmentOperators.js

---
+++ JS/expressions and operators/assigmentOperators.js
@@ -0,0 +1,31 @@
+// Operadores de atribuição
+
+let x
+
+x = 1
+
+console.log(x)
+
+x += 2 // x = x + 2
+ 
+console.log(x)
+
+x -= 1 // x = x - 1
+ 
+console.log(x)
+
+x *= 4 // x = x * 4
+ 
+console.log(x)
+
+x /= 2 // x = x / 2
+ 
+console.log(x)
+
+x %= 3 // x = x % 2
+ 
+console.log(x)
+
+x **= 2 // x = x ** 2
+ 
+console.log(x)
