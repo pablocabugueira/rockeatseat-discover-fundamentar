808bbdd7df55393093fae9ffe76c563aa061351a
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-07 22:43:18 -0300

    Create comparisonOperators2.js

---
+++ JS/expressions and operators/comparisonOperators2.js
@@ -0,0 +1,13 @@
+// Operadores de comparação
+// Compara valores e retorna um boolean como resultado
+// === Estritamente igual
+// !== Estritamente diferente
+
+let one = 1
+let two = 2
+
+console.log(one === "1") // False
+console.log(one == 1) // True
+
+console.log(two !== "2") // True
+console.log(two !== 2) // False
