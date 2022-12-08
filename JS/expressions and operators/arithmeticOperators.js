71c62c13a386b31422cf839acc9317de7449c5cc
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-07 22:32:58 -0300

    Create arithmeticOperators.js

---
+++ JS/expressions and operators/arithmeticOperators.js
@@ -0,0 +1,22 @@
+// Operadores Aritiméticos
+
+console.log(5 * 5) // Multiplicação *
+
+
+console.log(35 / 5) // Divisão /
+
+console.log(23 + 22) // Soma +
+
+console.log(10 - 2) // Subtração -
+
+console.log(10 % 3) // Resto da divisão (Remainder) %
+
+let increment = 0
+increment++
+console.log(increment) // Incremento (increment) ++
+
+let decrement = 0
+decrement--
+console.log(decrement) // Decremento (decrement) --
+
+console.log(3 ** 2) // Exponecial **
