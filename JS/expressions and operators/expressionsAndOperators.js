1119b1ded7d3ae4611a1f77039de8bf9d0ed14ac
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-07 22:09:37 -0300

    Create expressionsAndOperators.js

---
+++ JS/expressions and operators/expressionsAndOperators.js
@@ -0,0 +1,18 @@
+// Expressões e operadores
+
+let number = 1 // Expressão
+
+/*
+    Operators:
+    - Binary
+    - Unary
+    - Ternary
+*/
+
+console.log(number + 1) // Binary
+
+console.log(++number) // Unary
+console.log(--number) // Unary
+
+console.log(true ? "alo" : "oi") // Ternary
+console.log(false ? "alo" : "oi") // Ternary
