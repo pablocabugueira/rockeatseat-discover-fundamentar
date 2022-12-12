a90f32833d4b2f92947b8482bdbb160a490d5844
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-12 17:35:05 -0300

    Create ternaryConditionalOperator.js

---
+++ JS/expressions and operators/ternaryConditionalOperator.js
@@ -0,0 +1,18 @@
+// Operador Condicional Ternário
+
+let pao = true
+let queijo = true
+
+let niceBreakfast = pao && queijo ? "Café bom" : "Café ruim"
+console.log(niceBreakfast)
+
+let presunto = true
+let salame = false
+
+let niceBreakfast2 = presunto || salame ? "Café bom" : "Café ruim"
+console.log(niceBreakfast2)
+
+let age = 18
+
+let canDrive = age >= 18 ? "Can drive" : "Can't drive"
+console.log(canDrive)
