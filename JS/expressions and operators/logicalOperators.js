d353df1539fe1a223dd176a283be11ea458f4ebb
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-12 17:27:09 -0300

    Create logicalOperators.js

---
+++ JS/expressions and operators/logicalOperators.js
@@ -0,0 +1,10 @@
+// Operadores Lógicos
+
+let pao = true
+let queijo = false
+
+console.log(pao && queijo) // && and
+
+console.log(pao || queijo) // || or
+
+console.log(!pao) // ! not, troca o valor para o contrário
