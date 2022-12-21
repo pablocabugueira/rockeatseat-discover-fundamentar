3e7a1942bc73a5c61d95494bc3dfbc61e1b6ac1b
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-21 10:50:32 -0300

    Create forIn.js

---
+++ JS/repetitionStructure/forIn.js
@@ -0,0 +1,15 @@
+// for in
+
+let person = {
+    name: "Pablo",
+    age: 18,
+    weight: 65,
+}
+
+for(let property in person) {
+    console.log(person)
+}
+
+for(let property in person) {
+    console.log(person[property])
+}
