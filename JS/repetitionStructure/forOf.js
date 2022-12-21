77b4645a6ce119dfe68d2356d3d98917265f73ff
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2022-12-21 10:46:10 -0300

    Create forOf.js

---
+++ JS/repetitionStructure/forOf.js
@@ -0,0 +1,12 @@
+// for...of
+
+let name = "Pablo"
+let names = ["João", "Pedro", "Fernando"]
+
+for(let char of name) {
+    console.log(char)
+}
+
+for(let name of names) {
+    console.log(name)
+}
