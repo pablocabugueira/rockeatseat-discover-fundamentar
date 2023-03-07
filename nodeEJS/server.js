76971e545cafb50f95f8b21c81e6fbdec4de3330
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2023-03-06 23:18:44 -0300

    finished ejs

--- nodeEJS/server.js
+++ nodeEJS/server.js
@@ -4,11 +4,39 @@ const app = express()
 app.set("view engine", "ejs")
 
 app.get("/", function(req, res) {
-    res.render("index")
+    const items = [
+        {
+            title: "D",
+            message: "esenvolver aplicações"
+        },
+        {
+            title: "E",
+            message: "JS usa JavaScript para rodar HTML"
+        },
+        {
+            title: "M",
+            message: "uito fácil de usar"
+        },
+        {
+            title: "A",
+            message: "morzinho"
+        },
+        {
+            title: "I",
+            message: "nstall 'ejs'"
+        },
+        {
+            title: "S",
+            message: "intaxe simples"
+        },
+    ]
+    res.render("pages/index", {
+        qualities: items,
+    })
 })
 
 app.get("/sobre", function(req, res) {
-    res.render("about")
+    res.render("pages/about")
 })
 
 app.listen(8080)
